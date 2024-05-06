3f6f48510a7bf0f3c698f2a825c416cdd7efacdf
Brett Snyder <topwebworks@live.com>
2024-05-06 11:30:42 -0700

    Remove unneeded interaction from About page image.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,7 @@ import { Container } from '@/components/Container'
 import { GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
 
-import Tilt from 'react-parallax-tilt'
+// import Tilt from 'react-parallax-tilt'
 // import { RoughNotation, RoughNotationGroup } from 'react-rough-notation'
 
 function SocialLink({ className, href, children, icon: Icon }) {
@@ -45,17 +45,17 @@ export default function About() {
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
-            <Tilt className="mt-0 flex h-full w-full">
-              <div className="max-w-xs px-2.5 lg:max-w-none">
-                <Image
-                  src={portraitImage}
-                  alt="Brett Snyder"
-                  sizes="(min-width: 1024px) 32rem, 20rem"
-                  className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover grayscale-0 filter dark:bg-zinc-800"
-                  priority
-                />
-              </div>
-            </Tilt>
+            {/* <Tilt className="flex w-full h-full mt-0"> */}
+            <div className="max-w-xs px-2.5 lg:max-w-none">
+              <Image
+                src={portraitImage}
+                alt="Brett Snyder"
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover grayscale-0 filter dark:bg-zinc-800"
+                priority
+              />
+            </div>
+            {/* </Tilt> */}
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
