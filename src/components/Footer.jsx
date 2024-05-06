70cc8c2c04c71d34275b606c3dd4be2734c1a55e
Brett Snyder <topwebworks@live.com>
2024-05-05 21:18:27 -0700

    Edit Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,9 +17,9 @@ export function Footer() {
   return (
     <footer className="mt-32">
       <Container.Outer>
-        <div className="pt-10 pb-16 border-t border-zinc-100 dark:border-zinc-700/40">
+        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex flex-col items-center">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
@@ -27,8 +27,14 @@ export function Footer() {
                 <NavLink href="/uses">Uses</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Brett Snyder. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} Brett Snyder.
+                All&nbsp;rights&nbsp;reserved.
+              </p>
+              <p className="mx-auto max-w-[550px] pt-4 text-center text-[11px] text-zinc-400 dark:text-zinc-500">
+                <strong>Cookie Disclaimer:</strong> I use basic Google cookies
+                to follow country, and page view stats. No PII is collected.
+                BrettSnyder.me is a non-commercial site about me that does not
+                capture or sell lead&nbsp;information.
               </p>
             </div>
           </Container.Inner>
