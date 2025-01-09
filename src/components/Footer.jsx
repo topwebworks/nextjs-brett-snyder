855234d974288ca9cf2b6e283a56a61cea80c620
Brett Snyder <topwebworks@live.com>
2025-01-09 08:17:02 -0700

    Edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ export function Footer() {
   return (
     <footer className="mt-32">
       <Container.Outer>
-        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+        <div className="pt-10 pb-16 border-t border-zinc-100 dark:border-zinc-700/40">
           <Container.Inner>
             <div className="flex flex-col items-center">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
@@ -26,12 +26,12 @@ export function Footer() {
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/uses">Uses</NavLink>
               </div>
-              <p className="mx-auto w-full text-center text-xs text-zinc-400 dark:text-zinc-500 sm:text-sm">
+              <p className="w-full mx-auto text-xs text-center text-zinc-400 dark:text-zinc-500 sm:text-sm">
                 &copy; {new Date().getFullYear()} Brett Snyder.
                 All&nbsp;rights&nbsp;reserved.
               </p>
               <p className="mx-auto max-w-[750px] pt-4 text-center text-[11px] text-zinc-400 dark:text-zinc-500">
-                <strong>Cookie Disclaimer:</strong> I use basic Google cookies
+                <strong>Cookies:</strong> I use basic Google cookies
                 to follow country, and page view stats. YouTube and Chat cookies
                 pulled from Master Blocks article demos. No PII is collected.
                 BrettSnyder.me is a noncommercial site about me that does not
@@ -39,7 +39,7 @@ export function Footer() {
                 <a
                   id="cookie-preferences"
                   href="#"
-                  className="termly-display-preferences underline"
+                  className="underline termly-display-preferences"
                 >
                   Preferences
                 </a>
